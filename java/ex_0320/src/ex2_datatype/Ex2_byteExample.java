4e0b2119511fde4acb87b17e76ec4e4bab5a692f
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-20 18:27:15 +0900

    26.03.20 first study (4)

--- java/ex_0320/src/ex2_datatype/Ex2_byteExample.java
+++ java/ex_0320/src/ex2_datatype/Ex2_byteExample.java
@@ -6,6 +6,11 @@ public class Ex2_byteExample {
 		//정수의 경우 해당 자료형이 표현할 수 있는 범위를 벗어난 데이터를 저장하면
 		//오버,언더 플로우가 발생.
 		
+		byte var1 = (byte)-129;//언더 플로우
+		System.out.println(var1);
+		
+		byte var2 = (byte)128;// 오버 플로우
+		System.out.println(var2);
 	}
 
 }
