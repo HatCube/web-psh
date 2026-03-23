5e686d5f3e56664427312d4f58ff12bbaf248a92
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-23 15:00:32 +0900

    26.03.23 study

--- java/ex_0323/src/ex3_print/PrintExample.java
+++ java/ex_0323/src/ex3_print/PrintExample.java
@@ -23,7 +23,21 @@ public class PrintExample {
 		//논리형 %b > boolean
 		
 		
-		System.out.printf("제 이름은 %s 입니다, 제 나이는 %d세 입니다.",name,age);
+		System.out.printf("제 이름은 %s 입니다, 제 나이는 %d세 입니다.\n",name,age);
+		
+		
+		//원하는 소수점을 띄우고 싶다면 %(이곳)f 사이에 .(원하는 소수점)을 적을것
+		double height = 169.5;
+		System.out.printf("키는 %.1fcm입니다.\n",height);
+		
+		//빈공간 포함 6자리 수로 변환하는 방법은 %(이곳)d 사이에 원하는 숫자를 적을것
+		//또한 앞의 공백을 0으로 바꾸고 싶다면 위의 방법에 0을 추가로 앞에 적을것 ex)%03d
+		
+		
+		int price = 123;
+		System.out.printf("상품의 가격 :%6d원\n",price);
+		System.out.printf("상품의 가격 :%06d원\n",price);
+		System.out.printf("상품의 가격 :%-6d원\n",price);
 		
 		
 		
