fd0594b3f62e4e7a1690bc96564a7b59a97c7299
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-23 18:01:54 +0900

    26.03.23 study

---
+++ java/ex_0323/src/ex5_operator/BeatShiftOperatorExample.java
@@ -0,0 +1,10 @@
+package ex5_operator;
+
+public class BeatShiftOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- java/ex_0323/src/ex5_operator/LogicOperatorExample.java
+++ java/ex_0323/src/ex5_operator/LogicOperatorExample.java
@@ -35,12 +35,33 @@ public class LogicOperatorExample {
 		//피연산자가 1,0이라는 것과 산출 결과가 1,0이라는 점이 특징이다.
 		//1은 true , 0은 false라고 생각하면 논리연산자와 차이는 없다.
 		
+		// & 논리곱 : 둘다 1이면 1, 아니면 0
+		// | 논리합 : 둘중 하나라도 1이면 1,아니면 0
+		// ^ 배타적 논리합 : 두 항이 다르면 1, 같으면 0
+		// ~ 부정 : 1을 0으로 0을 1로 연산
 		
+		int a = 10; // 1010
+		int b = 7; // 0111
 		
+		int c = a & b; // 2
+		System.out.println("c : " + c);
 		
+		int a2 = 12;// 1100
+		int b2 = 8;// 1000
+		int c2 = a2 | b2;
+		System.out.println("c2 : " + c2);//1100
 		
+		int a3 = 9;//1001
+		int b3 = 11;//1011
+		int c3 = a3 ^ b3; //0010 = 2
+		System.out.println("c3 : " + c3);
 		
+		//~연산
+		//2진수는 음수를 표현할 수 없다.
+		//비트의 맨 앞자리 부호의 표현으로 쓰기로 약속했다.
 		
+		int a4 = 7;
+		System.out.println("~a4 : " + ~a4);
 		
 		
 		
