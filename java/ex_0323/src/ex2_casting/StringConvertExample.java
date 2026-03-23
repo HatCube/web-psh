3ca1e4cac33d14d5f981e9f10377b6ef8ec6221e
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-23 14:16:24 +0900

    26.03.23 study

---
+++ java/ex_0323/src/ex2_casting/StringConvertExample.java
@@ -0,0 +1,26 @@
+package ex2_casting;
+
+public class StringConvertExample {
+
+	public static void main(String[] args) {
+		
+		// 문자열 > 기본타입
+		int value1 = Integer.parseInt("10");
+		double value2 = Double.parseDouble("3.14");
+		boolean value3 = Boolean.parseBoolean("true");
+		
+		System.out.println(value1);
+		System.out.println(value2);
+		System.out.println(value3);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
