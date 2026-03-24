c1f13dfb19b0f9c0d1c20b7958bd71690b3f0509
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-24 18:19:19 +0900

    26.03.24 study

--- java/ex_0324/src/ex4_for/ForExample.java
+++ java/ex_0324/src/ex4_for/ForExample.java
@@ -1,5 +1,7 @@
 package ex4_for;
 
+import java.util.Scanner;
+
 public class ForExample {
 
 	public static void main(String[] args) {
@@ -48,15 +50,47 @@ public class ForExample {
 		
 		//정수형 변수를 하나 초기화 한다.
 		//해당 정수에 해당하는 구구단 출력하기
-		int num1;
-		int num2;
 		System.out.println("/-------------------------------------/");
 		
 		for (int i = 1 ; i <= 9 ; i++) {
 			System.out.println("3 X "+ i + " = " + (i * 3) + "");
 		} 
+		System.out.println("/-------------------------------------/");
+		int dan = 3;
+		//내가 반복하고 싶은것 곱하기 9번
+		for (int i = 1 ; i <= 9 ; i++) {
+			System.out.printf("%d X %d = %d\n",dan, i, dan*i);
+		} 
 		
+		System.out.println("/-------------------------------------/");
 		
+		//키보드에서 정수를 하나 입력 받고
+		//1부터 입력받은 정수까지의 총 합 구하기
+		//예를들어 5를 입력 받으면 1 ~ 5 까지의 총합인 15를 출력해야한다.
+		//1 + 2 + 3 + 4 + 5
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 입력 : ");
+		int num = sc.nextInt();
+		int total = 0;
+		
+		for (int i = 1; i <= num ; i++){
+			total += i;
+			System.out.println("" + total);
+			
+		}
+		//10개의 정수를 입력 받고 그 수들 중 짝수가 몇개인지 구하시오
+		
+		int count = 0;
+		
+		for (int i = 1 ; i <= 10 ; i++) {
+			System.out.println("정수값 입력 : ");
+			int num3 = sc.nextInt();
+			
+			if (num3 % 2 == 0) {
+				count++;
+			}
+			System.out.println("짝수의 개수 : " + count);
+		}
 		
 		
 		
