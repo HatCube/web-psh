3cbeebb7b418c6bcf08b0e9138acd8061a197829
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-24 17:10:19 +0900

    26.03.24 study

---
+++ java/ex_0324/src/ex4_for/ForExample.java
@@ -0,0 +1,37 @@
+package ex4_for;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <=3; i++) {
+			System.out.println(i + "");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
