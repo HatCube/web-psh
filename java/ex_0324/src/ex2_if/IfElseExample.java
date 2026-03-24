7cc1323af80af861bd30dfcb8f27e27f81d11adc
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-24 13:36:51 +0900

    26.03.24 study

---
+++ java/ex_0324/src/ex2_if/IfElseExample.java
@@ -0,0 +1,30 @@
+package ex2_if;
+
+public class IfElseExample {
+	public static void main(String[] args) {
+		
+		int age = 17;
+		
+		if (age >= 20){
+			
+			System.out.println("성인입니다");
+		}else {
+			System.out.println("미성년자 입니다");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
