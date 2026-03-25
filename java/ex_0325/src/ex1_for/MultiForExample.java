73ac6781b30d49a61bdbf53f147846b73e74f65e
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-25 11:39:21 +0900

    26.03.25 study

--- java/ex_0325/src/ex1_for/MultiForExample.java
+++ java/ex_0325/src/ex1_for/MultiForExample.java
@@ -6,11 +6,16 @@ public class MultiForExample {
 		
 		for(int i = 1; i <= 3; i++) {
 			
-			
-			
-			
-			
+			for(int j = 1; j <= 3; j++) {
+				System.out.println(i + "" + j);
+			}
+		}
+			int count = 1;
+			for(int i = 1; i <= 3 ;i++) {
+				for (int j = 1; j <= 4; j++) {
+					System.out.printf("%02d",count++);
+				}System.out.println();
+			}		
 		}
 	}
-
-}
+	
