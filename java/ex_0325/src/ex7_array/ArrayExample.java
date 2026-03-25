87c130a9b8b54de95ca797fd56c130d97566f685
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-25 18:05:26 +0900

    26.03.25 study

---
+++ java/ex_0325/src/ex7_array/ArrayExample.java
@@ -0,0 +1,20 @@
+package ex7_array;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		//{1,2,3,4,5}; > 초기화 리스트
+		//배열을 선언함과 동시에 초기화 리스트를 사용하는것이 가능하다.
+		
+		int[] arr = {1,2,3,4,5};
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
