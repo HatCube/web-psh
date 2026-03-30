1ac4b08963692e5ab5c91a0d26f9283cb369099a
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-30 17:48:08 +0900

    26.03.30 study

---
+++ java/ex_0330/src/ex6_inheritance/Phone.java
@@ -0,0 +1,34 @@
+package ex6_inheritance;
+//상속해주는 클래스(부모 클래스, 슈퍼클래스)
+//작식 클래스에게 필드 메서드를 사용할 수 있도록 제공
+public class Phone {
+	
+	public String model;
+	public String color;
+	
+	public void bell() {
+		System.out.println("벨이 울립니다.");
+	}
+	
+	public void sendMassage(String message) {
+		System.out.println("나 : " + message);
+	}
+	
+	public void receiveMassage(String message) {
+		System.out.println("상대방 : " + message);
+	}
+	
+	public void hangUp() {
+		System.out.println("전화를 끊습니다.");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
