66e431220a3824f2b0abcc40e6d9a0fdc204d243
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-30 14:09:36 +0900

    26.03.30 study

--- java/ex_0330/src/ex3_access/method/package1/B.java
+++ java/ex_0330/src/ex3_access/method/package1/B.java
@@ -2,5 +2,19 @@ package ex3_access.method.package1;
 
 //같은 패키지의 다른 클래스
 public class B {
-
+	
+	public void method() {
+	A a = new A();
+	
+	a.field1 = 1;
+	a.field2 = 1;
+	//a.field3 = 1; private 라서 불가능.
+	
+	a.method1();
+	a.method2();
+	//a.method3(); 위의 private와 같은 이유.
+	
+	
+	
+	}
 }
