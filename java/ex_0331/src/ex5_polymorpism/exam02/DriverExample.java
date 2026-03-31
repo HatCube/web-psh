db5a67870445e993c11395ed686abc1f925ffe15
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-03-31 16:58:35 +0900

    26.03.31 study

---
+++ java/ex_0331/src/ex5_polymorpism/exam02/DriverExample.java
@@ -0,0 +1,8 @@
+package ex5_polymorpism.exam02;
+
+public class DriverExample {
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		driver.drive(new Taxi());
+	}
+}
---
+++ java/ex_0331/src/ex5_polymorpism/exam02/Vehicle.java
@@ -0,0 +1,12 @@
+package ex5_polymorpism.exam02;
+
+public class Vehicle {
+	
+	public void run() {
+		System.out.println("차량이 달립니다.");
+	}
+	
+	
+	
+	
+}
