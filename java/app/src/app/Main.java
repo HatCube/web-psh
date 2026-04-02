6a8bd08ab5c7452cc404437c20e7c504d96d3a8c
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-04-02 18:27:13 +0900

    26.04.02 study

---
+++ java/app/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.util.Scanner;
+import member.Member;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("사용자의 이름 : ");
+		sc.next();
+		
+		System.out.println("사용자의 나이 : ");
+		sc.nextInt();
+		
+		
+	}
+}
