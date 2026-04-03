bf2e89ee3edc93251e5a20f3fa88e8ca8335f85c
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-04-03 10:38:44 +0900

    26.04.03 study

--- java/app/src/app/Main.java
+++ java/app/src/app/Main.java
@@ -2,17 +2,19 @@ package app;
 
 import java.util.Scanner;
 import member.Member;
+import service.MemberService;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("사용자의 이름 : ");
-		sc.next();
+		System.out.println("사용자의 이름 입력 : ");
+		String name = sc.next();
 		
-		System.out.println("사용자의 나이 : ");
-		sc.nextInt();
+		System.out.println("사용자의 나이 입력 : ");
+		int age = sc.nextInt();
 		
-		
+		Member member = new Member();
+		MemberService ms = new MemberService();
 	}
 }
--- java/service/src/module-info.java
+++ java/service/src/module-info.java
@@ -1,4 +1,4 @@
 module service {
 	requires member;
-
+	exports service;
 }
