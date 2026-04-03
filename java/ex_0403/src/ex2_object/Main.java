f06214e43b52ca49de5c9fb01f49162dcc3984ca
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-04-03 18:11:30 +0900

    26.04.03 study

--- java/ex_0403/src/ex2_object/Main.java
+++ java/ex_0403/src/ex2_object/Main.java
@@ -9,12 +9,26 @@ public class Main {
 		Product[] product = {p1,p2,p3};
 		
 		
-		for(Product p : product) {
-			System.out.println(p);
+		//for(Product p : product) {
+			//System.out.println(p); 꼬임 발생으로 인해 잠시 격리
 		
 		Object[] arr = {"Java",100,3.14, new Employee("홍길동","개발팀")};
 		
+		//반복문으로 배열을 순회하면서
+		//문자열이면 "문자열 데이터 : xx"
+		//정수면 "정수 데이터 : xx;
+		//실수면 "실수 데이터 : xx"
+		//Employee객체면 이름과 부서를 출력해주세요
 		
+		for(Object o: arr) {
+			if(o instanceof String) System.out.println("문자열 데이터 : " + o);
+			else if(o instanceof Integer) System.out.println("정수데이터 : " + o);
+			else if(o instanceof Double) System.out.println("실수데이터 : " + o);
+			else if(o instanceof Employee) {
+				Employee e = (Employee)o;
+				System.out.print("사원명 : " + e.name + "부서 : " + e.dept);
+			}
+		}
 		
 		
 		
@@ -30,4 +44,3 @@ public class Main {
 		
 		}
 	}
-}
