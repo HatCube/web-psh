7cb3be618122581c18c0e38239260270e28eb7e4
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-04-07 15:38:33 +0900

    26.04.07 study

--- java/ex_0407/src/ex3_generic/DataSampleExample.java
+++ java/ex_0407/src/ex3_generic/DataSampleExample.java
@@ -13,6 +13,10 @@ public class DataSampleExample {
 		//배열에 들어있는 내용 출력
 		for(int i = 0; i < list.size(); i++) {
 			Object data = list.get(i);
+			
+			//저장 데이터 타입이 Object이므로 어떤 타입의 데이터라도
+			//저장할 수 있지만 데이터를 사용할 때는 타입 변환을 위한
+			//검사를 해야 하는 번거로움이 있다.
 			if(data instanceof Integer) {
 				System.out.println("정수 : " + (int)data);
 			}else if (data instanceof Double) {
