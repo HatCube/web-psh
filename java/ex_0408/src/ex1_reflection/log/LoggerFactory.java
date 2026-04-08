7b664ce8661dcc13bce96b88b4c463042ff3dbd2
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-04-08 11:47:02 +0900

    26.04.08 study

--- java/ex_0408/src/ex1_reflection/log/LoggerFactory.java
+++ java/ex_0408/src/ex1_reflection/log/LoggerFactory.java
@@ -1,5 +1,41 @@
 package ex1_reflection.log;
 
-public class LoggerFactory {
+import java.io.FileInputStream;
+import java.util.Properties;
 
+
+
+//리플렉션 + 설정파일
+//설정파일을 읽고, 리플렉션으로 객체를 생성해서 반환하는 공장역할
+
+//로그를 출력하는 프로그램을 만들되,로그 쿨력방식은 코드가 아니라 설정파일에 따라서 바뀌게 만들기 위함
+
+//개발중에는 콘솔에 출력
+//운영환경에서는 파일에 저장
+public class LoggerFactory {
+	
+	public static Logger getLogger() {
+		//properties : 설정파일을 읽어오기 위한 클래스
+		Properties prop = new Properties();
+		try {
+			//load() : 설정파일을 읽어온다.
+			prop.load(new FileInputStream("config.properties"));
+			
+			//클래스 이름 가져오기
+			String className = prop.getProperty("loggerClass");
+			
+			//리플렉션을 이용하여 클래스를 찾는다.
+			Class clazz = Class.forName(className);
+			
+			//객체를 만든다.
+			//getDeclaredConstructor() : 클래스에 선언된 생성자를 가져오는 기능
+			//newInstance() : 그 생성자를 실행해 객체를 만든다.
+			Object obj = clazz.getDeclaredConstructor().newInstance();
+			
+			return(Logger)obj;
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Logger 생성 실패",e);
+		}
+	}
 }
