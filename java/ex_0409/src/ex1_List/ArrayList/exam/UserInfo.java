7aef6f91f88516658ed03dceffc621cf019f8429
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-04-09 18:22:53 +0900

    26.04.09 study

---
+++ java/ex_0409/src/ex1_List/ArrayList/exam/UserInfo.java
@@ -0,0 +1,22 @@
+package ex1_List.ArrayList.exam;
+
+public class UserInfo {
+	
+	private String id;
+	private int pswd;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getPswd() {
+		return pswd;
+	}
+	public void setPswd(int pswd) {
+		this.pswd = pswd;
+	}
+	
+	
+}
