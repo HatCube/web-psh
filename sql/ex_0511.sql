5496cac70f9dcaab0c5f6bd3d857a5c06e227437
mokdoridambi <69251769+HatCube@users.noreply.github.com>
2026-05-11 17:46:40 +0900

    26.05.11 study

--- sql/ex_0511.sql
+++ sql/ex_0511.sql
@@ -25,40 +25,126 @@ CREATE TABLE orders (
     order_date DATETIME DEFAULT NOW()
 );
 
--- 상품 ID로 상품 조회하기
--- 이름 : get_product_by_id
--- 상품 id를 전달받아서 해당 상품 정보를 조회하세요
+-- 전체 상품 조회 프로시저 만들기
+-- 이름 : get_all_procedure
+-- product 테이블의 전체 상품을 조회한다.
+-- 가격이 높은 순서대로 정렬한다.
 
 delimiter //
-create procedure get_product_by_id(in p_id int)
+create procedure get_all_procedure()
 begin
-	select * 
+	select *
 	from product
-	where product_id = p_id;
+	order by price desc;
 end //
 delimiter ;
 
+call get_all_procedure();
+
+-- 상품 ID로 상품 조회하기
+-- 이름 : get_product_by_id
+-- 상품 id를 전달받아서 해당 상품 정보를 조회하세요
+delimiter //
+create procedure get_product_by_id(
+	in p_product_id int
+)
+begin
+	select *
+	from product
+	where product_id = p_product_id;
+end //
+delimiter ;
+
+call get_product_by_id(1);
 -- 카테고리별 상품 조회하기
 -- 이름 : get_products_by_category
 -- 카테고리명을 입력받는다.
 -- 해당 카테고리의 상품만 조회한다.
 -- 가격이 낮은 순으로 정렬한다.
+delimiter //
+create procedure get_products_by_category(
+	in p_category varchar(50)
+)
+begin
+	select *
+	from product
+	where category = p_category
+	order by price;
+end //
+delimiter ;
 
-
+call get_products_by_category('전자기기');
 
 -- 상품 재고를 반환받기
 -- 이름 : get_product_stock
 -- 상품 ID를 입력받는다.
 -- 해당 상품의 재고를 OUT 매개변수로 반환한다.
 -- 호출 후 세션 변수로 결과를 확인한다.
+delimiter //
+create procedure get_product_stock(
+	in p_product_id int,
+	out p_stock int
+)
+begin
+	select stock
+	into p_stock
+	from product
+	where product_id = p_product_id;
+end //
+delimiter ;
 
--- 상품 가격 수정하기
+call get_product_stock(1, @result_stock);
+select @result_stock as stock;
+
+-- 상품 추가 프로시저 만들기
+-- 이름 : insert_product
+-- 상품명, 카테고리, 가격, 재고를 입력받아서 product 테이블에 상품을 추가한다.
+-- 추가 후 전체 상품 목록을 조회한다.
+
+drop procedure insert_product;
+
+delimiter //
+create procedure insert_product(
+	in p_product_name varchar(100),
+	in p_category varchar(50),
+	in p_price int,
+	in p_stock int
+)
+begin
+	insert into product (product_name, category, price, stock)
+	values (p_product_name, p_category, p_price, p_stock);
+
+	select * from product;
+end //
+delimiter ;
+
+call insert_product('웹캠','전자기기',80000,25);
+
+--  상품 가격 수정하기
 -- 이름 : update_product_price
--- 상품 : 상품 id와 변경할 가격을 입력받는다.
+-- 상품 ID와 변경할 가격을 입력받는다.
 -- 해당 상품의 가격을 수정한다.
--- 수정된 행이 있으면 '가격이 수정되었습니다' 출력
--- 수정된 행이 없으며 '해당 상품이 없습니다' 출력
--- ROW_COUNT() : 변경된 행의 개수 
+-- 수정된 행이 있으면 "가격이 수정되었습니다." 출력하기
+-- 수정된 행이 없으면 "해당 상품이 없습니다." 출력하기
+-- ROW_COUNT() : 변경된 행의 개수 반환
+delimiter //
+create procedure update_product_price(
+	in p_product_id int,
+	in p_price int
+)
+begin
+	update PRODUCT
+	set PRICE = P_PRICE
+	where PRODUCT_ID = P_PRODUCT_ID;
+
+	-- row_count() : 직전에 실행한 sql문이 몇개의 행에 영향을 주었는지 반환
+	if ROW_COUNT() > 0 then
+		select '가격이 수정되었습니다.' as message;
+	else
+		select '해당 상품이 없습니다.' as message;
+	end if;
+end //
+delimiter ;
 
 -- 상품이 있으면 수정하고 없으면 추가하기
 -- 이름 upsert_product
@@ -271,3 +357,284 @@ select
 		else '기타 코너'
 	end as display_area
 	from product;
+
+-- case문은 select 절에서 사용되며 실제 테이블에 없는 가상의 결과 컬럼을 만들 수 있다.
+
+-- case문은 order by에서도 사용할 수 있다.
+select 
+	product_name,
+	stock
+from product
+order by
+	case 
+		when stock < 10 then 1
+		when stock < 30 then 2
+		else 3
+	end, stock;
+	
+-- update 문에서도 사용할 수 있다.
+alter table product add status varchar(20);
+
+update product
+set status = 
+	case
+		when stock <= 10 then '재고 부족'
+		else '판매중'
+	end;
+
+select * from product;
+
+-- group by와 집계에도 자주 사용된다.
+-- 상품이 가격대별로 몇개 씩 있는지 조회하기
+select
+	case 
+		when price >= 100000 then '고가'
+		when price >= 50000 then '중가'
+		else '저가'
+	end as price_level,
+	count(*) as product_count
+	from product
+	group by
+		case 
+			when price >= 100000 then '고가'
+			when price >= 50000 then '중가'
+			else '저가'
+	end;
+
+-- sum과 case 를 같이 쓰면 조건별 집계가 가능하다.
+-- 조건별 개수를 직접 계산한다.
+select
+	sum(case when category = '전자기기' then 1 else 0 end) as electronic_count,
+	sum(case when category = '가구' then 1 else 0 end) as furniture_count,
+	sum(case when category = '생활용품' then 1 else 0 end) as life_count
+	from product;
+
+-- 카테고리별로 그룹을 만든다.
+select 
+	category,count(*)
+	from product p
+	group by category;
+
+CREATE TABLE student_score (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(30),
+    subject VARCHAR(30),
+    score INT,
+    attendance_rate INT
+);
+INSERT INTO student_score VALUES
+(1, '김철수', 'Java', 95, 98),
+(2, '이영희', 'Java', 82, 90),
+(3, '박민수', 'Java', 67, 75),
+(4, '최유리', 'Database', 88, 92),
+(5, '정하늘', 'Database', 73, 80),
+(6, '한지민', 'Database', 55, 60),
+(7, '강민호', 'Python', 100, 100),
+(8, '윤서연', 'Python', 45, 50);
+
+-- 점수 등급 출력하기
+-- 90점 이상 A 
+-- 80점 이상 B
+-- 70점 이상 C 
+-- 60점 이상 D
+-- 그 외 F
+-- 출력 컬럼 student_name score grade
+
+select 
+	student_name,
+	score,
+	case 
+		when score >= 90 then 'A'
+		when score >= 80 then 'B'
+		when score >= 70 then 'C'
+		when score >= 60 then 'D'
+		else 'F'
+	end as grade
+from student_score;
+
+-- 출석상태 출력하기
+-- 95 이상 우수
+-- 80 이상 보통
+-- 그외 위험
+-- 출력 컬럼 
+-- 이름 출석률 출석 상태
+
+select 
+	student_name,
+	attendance_rate,
+	case 
+		when attendance_rate >= 95 then '우수'
+		when attendance_rate >= 80 then '보통'
+		else '위험'
+	end as '출석상태'
+from student_score;
+
+-- 과목별 난이도 표시하기
+-- java > 중급
+-- database > 어려움
+-- Python 쉬움
+-- 출력 컬럼 
+-- subject difficulty
+
+select 
+	subject,
+	case subject
+		when 'Java' then '중급'
+		when 'Database' then '어려움'
+		when 'Python' then '쉬움'
+	end as difficulty
+from student_score;
+
+-- 장학생 여부 출력
+-- 점수 >= 90이고 출석률 >= 90
+-- 둘다 만족하면 장학생 
+-- 아니면 일반학생
+
+select 
+	student_name,
+	score,
+	attendance_rate,
+	case 
+		when score >= 90 and attendance_rate >= 90 then '장학생'
+		else '일반학생'
+	end as scholarship
+	from student_score;
+	
+
+-- grade 컬럼을 추가한 후 다음 기준으로 값을 update 하기
+-- 90점 이상 A
+-- 80점 이상 B
+-- 70점 이상 C
+-- 60점 이상 D
+-- 그 외 F 
+
+alter table student_score add grade varchar(20);
+
+update student_score
+set grade = 
+		case 
+			when score >= 90 then 'A'
+			when score >= 80 then 'B'
+			when score >= 70 then 'C'
+			when score >= 60 then 'D'
+			else 'F'
+		end;
+
+select * from student_score ss;
+
+-- while문의 기본 문법
+-- while 조건 do
+-- 실행문;
+-- end while;
+
+create table loop_test(
+	num int
+);
+delimiter //
+create procedure while_example()
+begin
+	declare v_num int default 1;
+	
+	while v_num <= 5 do
+		insert into loop_test values(v_num);
+		set v_num = v_num + 1;
+	end while;
+end //
+delimiter ;
+
+call while_example();
+select * from loop_test;
+
+-- repeat
+-- 무조건 한 번 실행 후 조건 검사
+-- repeat
+-- 실행문;
+-- until 조건
+-- end repeat;
+
+delimiter //
+create procedure repeat_example()
+begin
+	declare v_num int default 6;
+	
+	repeat
+		insert into loop_test values(v_num);
+		set v_num = v_num + 1;
+	until v_num > 10 -- 조건이 거짓이면 반복, 참이면 종료
+	end repeat;
+end //
+delimiter ;
+
+call repeat_example;
+
+select * from loop_test;
+
+-- loop
+-- 무한반복 기반
+-- 직접 종료 조건을 만들어야 한다.
+-- label : loop
+-- 실행문 ;
+-- if 조건 then leave label;
+-- end if;
+-- end loop;
+
+delimiter //
+create procedure loop_example()
+begin
+	declare v_num int default 11;
+	
+	my_loop:loop
+		insert into loop_test values(v_num);
+		
+		set v_num = v_num + 1;
+		
+		if v_num > 16 then
+			leave my_loop;
+		end if;
+	end loop;
+end //
+delimiter ;
+
+call loop_example;
+
+select * from loop_test;
+
+create table loop_result(
+	value_text varchar(100)
+);
+-- while 문을 이용하여 1 - 20 까지의 수 중 짝수만 저장하라
+delimiter //
+create procedure even_num()
+begin
+	declare v_num int default 1;
+	
+		while v_num <= 20 do
+			if mod(v_num,2) = 0 then 
+				insert into loop_result values(v_num);
+			end if ;
+		set v_num = v_num + 1;
+	end while;
+end //
+delimiter ;
+
+call even_num();
+select * from loop_result;
+
+-- repeat 문을 사용하여 1 부터 10 까지의 총합 구하기
+
+delimiter //
+create procedure sum_num()
+begin
+	declare v_num int default 1;
+	declare v_sum int default 0;
+
+		repeat
+			set v_sum = v_sum + v_num;
+			set v_num = v_num + 1;
+		until v_num > 10
+	end repeat;
+		select v_sum;
+end //
+delimiter ;
+
+call sum_num;
